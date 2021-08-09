25f4315b5f9c7edbb7afaf2f6b516e5aad1c1c96
Venktesh Soma <venkteshs@cybage.com>
2021-08-09 16:28:24 +0530

    Displayed the api data on the Cards i.e Real time covid-19 data

---
+++ src/components/CardDisplay.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import moment from "moment";
+import { Card, Col, Row } from "antd";
+import { Typography } from "antd";
+import styles from "./CardDisplay.module.css";
+
+function CardDisplay({ title, covidStatus, covidStatusClassName, message }) {
+  const { Title } = Typography;
+
+  let format = "ddd MMM Do YYYY";
+  let timeFormat = " h:mm:ss a";
+  let formattedDate = moment().format(format, covidStatus?.lastUpdate);
+  let formattedTime = moment().format(timeFormat, covidStatus?.lastUpdate);
+
+  // conditional classNames
+  let covidStatusLabel = "";
+  if (covidStatusClassName === "confirmed") {
+    covidStatusLabel = styles.confirmed;
+  }
+
+  if (covidStatusClassName === "recovered") {
+    covidStatusLabel = styles.recovered;
+  }
+
+  if (covidStatusClassName === "deaths") {
+    covidStatusLabel = styles.deaths;
+  }
+
+  if (covidStatusClassName === "active") {
+    covidStatusLabel = styles.active;
+  }
+
+  return (
+    <div className={styles["site-card-border-less-wrapper"]}>
+      <Col span={8}>
+        <Card
+          title={title}
+          bordered={false}
+          style={{ width: 240 }}
+          className={covidStatusLabel}
+          hoverable
+        >
+          {/* <h2>{returnTotalConfirmedData()}</h2> */}
+          {/* <h2>{covidStatus.confirmed?.value}</h2> */}
+          <div className={styles.cardContent}>
+            <Title level={3}>{covidStatus}</Title>
+            <div className={styles.lastUpdated}>Last updated at : </div>
+            <div className={styles.date}>{formattedDate}</div>
+            <div>{formattedTime}</div>
+            <Title level={5}>{message}</Title>
+          </div>
+        </Card>
+      </Col>
+    </div>
+  );
+}
+
+export default CardDisplay;
