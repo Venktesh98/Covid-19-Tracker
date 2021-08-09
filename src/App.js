6c32bc1d9e319637e4b26d6b02b149bd443ec299
Venktesh Soma <venkteshs@cybage.com>
2021-08-09 18:26:45 +0530

    Modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./App.module.css";
-import Card from "./components/ShowCard";
+import Card from "./components/Cards/CardInfo";
 
 function App() {
   return (
