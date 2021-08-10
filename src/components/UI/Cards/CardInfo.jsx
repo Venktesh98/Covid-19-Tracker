8e64c4a6d839466bb0ca75314022dcafa54cba53
Venktesh Soma <venkteshs@cybage.com>
2021-08-10 13:58:13 +0530

    Removed all the conditions that are previoulsy used for displaying the cards

--- src/components/UI/Cards/CardInfo.jsx
+++ src/components/UI/Cards/CardInfo.jsx
@@ -5,86 +5,115 @@ import CardDisplay from "./CardDisplay";
 import styles from "./CardInfo.module.css";
 import { Row } from "antd";
 
-function ShowCard({ covidStatus, active }) {
+function CardInfo({ covidStatus }) {
+  console.log("covidStatus:", covidStatus);
   // object Destructuring
   const { confirmed, recovered, deaths } = covidStatus;
 
-  const displayCardByCriteria = () => {
-    let keyNames = Object.keys(covidStatus);
+  // Old Way of Displaying Data
+  // const displayCardByCriteria = () => {
+  //   let keyNames = Object.keys(covidStatus);
 
+  //   const totalActiveCases = {
+  //     value: confirmed?.value - recovered?.value - deaths?.value,
+  //   };
+
+  //   let newKeyNames = [...keyNames, active];
+
+  //   return newKeyNames.map((keyName, index) => {
+  //     if (keyName === "confirmed") {
+  //       return (
+  //         <CardDisplay
+  //           key={index}
+  //           title="Infected"
+  //           covidStatus={confirmed?.value}
+  //           covidStatusClassName={keyName}
+  //           message="Total Infected Cases of Covid-19 Currently"
+  //         />
+  //       );
+  //     }
+
+  //     if (keyName === "recovered") {
+  //       return (
+  //         <CardDisplay
+  //           key={index}
+  //           title="Recovered"
+  //           covidStatus={recovered?.value}
+  //           covidStatusClassName={keyName}
+  //           message="Total Recovered Cases of Covid-19 Currently"
+  //         />
+  //       );
+  //     }
+
+  //     if (keyName === "deaths") {
+  //       return (
+  //         <CardDisplay
+  //           key={index}
+  //           title="Deaths"
+  //           covidStatus={deaths?.value}
+  //           covidStatusClassName={keyName}
+  //           message="Total Deaths of Covid-19 Currently"
+  //         />
+  //       );
+  //     }
+
+  //     if (keyName === "active") {
+  //       return (
+  //         <CardDisplay
+  //           key={index}
+  //           title="Active"
+  //           covidStatus={totalActiveCases?.value}
+  //           covidStatusClassName={keyName}
+  //           message="Total Active Cases of Covid-19 Currently"
+  //         />
+  //       );
+  //     }
+  //   });
+  // };
+
+  const displayCardByCriteria = () => {
     const totalActiveCases = {
       value: confirmed?.value - recovered?.value - deaths?.value,
     };
 
-    let newKeyNames = [...keyNames, active];
-
-    return newKeyNames.map((keyName, index) => {
-      if (keyName === "confirmed") {
-        return (
-          <CardDisplay
-            key={index}
-            title="Infected"
-            covidStatus={confirmed?.value}
-            covidStatusClassName={keyName}
-            message="Total Infected Cases of Covid-19 Currently"
-          />
-        );
-      }
-
-      if (keyName === "recovered") {
-        return (
-          <CardDisplay
-            key={index}
-            title="Recovered"
-            covidStatus={recovered?.value}
-            covidStatusClassName={keyName}
-            message="Total Recovered Cases of Covid-19 Currently"
-          />
-        );
-      }
-
-      if (keyName === "deaths") {
-        return (
-          <CardDisplay
-            key={index}
-            title="Deaths"
-            covidStatus={deaths?.value}
-            covidStatusClassName={keyName}
-            message="Total Deaths of Covid-19 Currently"
-          />
-        );
-      }
-
-      if (keyName === "active") {
-        return (
-          <CardDisplay
-            key={index}
-            title="Active"
-            covidStatus={totalActiveCases?.value}
-            covidStatusClassName={keyName}
-            message="Total Active Cases of Covid-19 Currently"
-          />
-        );
-      }
-    });
+    return (
+      <>
+        <CardDisplay
+          title="Infected"
+          covidStatus={confirmed?.value}
+          covidStatusClassName={styles.confirmed}
+          message="Total Infected Cases of Covid-19 Currently"
+        />
+        ,
+        <CardDisplay
+          title="Recovered"
+          covidStatus={recovered?.value}
+          covidStatusClassName={"recovered"}
+          message="Total Recovered Cases of Covid-19 Currently"
+        />
+        ,
+        <CardDisplay
+          title="Deaths"
+          covidStatus={deaths?.value}
+          covidStatusClassName={"deaths"}
+          message="Total Deaths of Covid-19 Currently"
+        />
+        ,
+        <CardDisplay
+          title="Active"
+          covidStatus={totalActiveCases?.value}
+          covidStatusClassName={"active"}
+          message="Total Active Cases of Covid-19 Currently"
+        />
+      </>
+    );
   };
-
-  // const returnTotalConfirmedData = () => {
-  //   return confirmedData?.map((confirmedTotalCases, index) => {
-  //     if (confirmedData.length - 1 === index) {
-  //       console.log("In IF:", confirmedTotalCases.confirmed.total);
-  //       return confirmedTotalCases.confirmed.total;
-  //     }
-  //     // console.log("Demo:",confirmedTotalCases.confirmed.total);
-  //   });
-  // };
 
   return (
     <div className={styles.cardBgColor}>
       <Row justify="center">{displayCardByCriteria()}</Row>
-      {/* <CardDisplay infectedData={infectedData} /> */}
     </div>
   );
 }
 
-export default ShowCard;
+export default CardInfo;
