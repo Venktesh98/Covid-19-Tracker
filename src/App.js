ab1175d54a357e7d25c695bd0ff53ed9a714d925
Venktesh Soma <venkteshs@cybage.com>
2021-08-10 12:21:04 +0530

    Modified

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./App.module.css";
-import Card from "./components/Cards/CardInfo";
+import {
+  fetchAllCovidData,
+  fetchAllCountries,
+} from "./components/Services/useAxios";
+import Card from "./components/UI/Cards/CardInfo";
+import Countries from "./components/UI/DropDown/Countries";
 
 function App() {
+  const [covidStatus, setCovidStatus] = useState("");
+  const [active, setActive] = useState("");
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    // grabs all covid-19 Data worldwide
+    fetchAllCovidData()
+      .then((response) => {
+        console.log("Response:", response.data);
+        setCovidStatus(response?.data);
+        setActive("active");
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+      });
+
+    // grabs all countries around worldwide
+    fetchAllCountries()
+      .then((response) => {
+        console.log("Country:", response);
+        setCountries(response.data.countries)
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+      });
+  }, []);
+
   return (
     <div className={styles.main}>
       <div>
@@ -11,7 +43,8 @@ function App() {
           className={styles.covidImage}
         />
       </div>
-      <Card />
+      <Card covidStatus={covidStatus} active={active} />
+      <Countries countries={countries} />
     </div>
   );
 }
