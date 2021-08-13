0ae99f54d1fef130cd309ee021b7255fba5b413c
Venktesh98 <venkysoma1998@gmail.com>
2021-08-13 11:34:45 +0530

    Rendered the graph globally using condition

--- src/components/UI/Charts/ChartDisplay.jsx
+++ src/components/UI/Charts/ChartDisplay.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import styles from "./ChartDisplay.module.css";
 import { Doughnut } from "react-chartjs-2";
 import { colors } from "../../Services/colors";
+import { Typography } from "antd";
 
-function ChartDisplay({ covidStatus, activeCasesData }) {
+function ChartDisplay({ covidStatus, activeCasesData, selectedCountry }) {
+  const { Title } = Typography;
   console.log("In Chart");
   const { confirmed, recovered, deaths } = covidStatus;
 
@@ -20,24 +22,30 @@ function ChartDisplay({ covidStatus, activeCasesData }) {
         ],
         backgroundColor: colors.bgColors,
         borderColor: colors.borderColors,
+        borderWidth: 1,
       },
     ],
   };
 
   return (
     <div className={styles.doughnutContainer}>
-      <div className={styles.doughnut}>
-        <Doughnut
-          data={data}
-          options={{
-            responsive: true,
-            maintainAspectRatio: true,
-          }}
-        />
-        {/* <div className={styles.coronaImageContainer}>
+      {selectedCountry ? (
+        "COUNTRY"
+      ) : (
+        <div className={styles.doughnut}>
+          <Doughnut
+            data={data}
+            options={{
+              responsive: true,
+              maintainAspectRatio: true,
+            }}
+          />
+          <Title level={5}>Worldwide Covid-19 Data</Title>
+          {/* <div className={styles.coronaImageContainer}>
           <img src={process.env.PUBLIC_URL + "/corona.jpg"} className={styles.coronaImage} />
         </div> */}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
